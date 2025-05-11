41cb4e107a910cdbbba17dcf6d71628a3f1964cd
CreepyFiN <veeti.velin@gmail.com>
2025-05-11 17:51:19 +0300

    More fixes

--- main.cpp
+++ main.cpp
@@ -47,18 +47,26 @@ field init_game(){
     //Käyttäjäsyöte kentän kokoa ja miinoja varten
     pair<int,int> size;
     int mines;
-    cout << "Give height and width of field and amount of mines: ";
-    if(scanf("%d %d %d", &size.first, &size.second, &mines) != 3){
+    while (true) {
+        cout << "Give height and width of field and amount of mines: ";
+        string line;
+        getline(cin >> ws, line);
+        if (sscanf(line.c_str(), "%d %d %d", &size.first, &size.second, &mines) == 3) {
+            break; // Päästään ulos loopista kun syöte onnistuu
+        }
         input_error();
-        return init_game();
     }
     pair<int,int> coord;
     // Väliaikainen käyttäjäsyöte, kunnes saadaan graafinen käyttöliittymä
-    cout << "Give starting coordinates: ";
-    if(scanf("%d %d", &coord.first, &coord.second) != 2){
+    while (true) {
+        cout << "Give starting coordinates: ";
+        string line;
+        getline(cin >> ws, line);
+        if (sscanf(line.c_str(), "%d %d", &coord.first, &coord.second) == 2) {
+            break; // Päästään ulos loopista kun syöte onnistuu
+        }
         input_error();
-        return init_game();
-    };
+    }
     // Luodaan kenttä
     field plot = create_field(size.first,size.second,mines,coord);
     reveal_tiles(plot, coord, false);
